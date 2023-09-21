ba3b5c4bfd38b8731dd922959e6e5efb24053734
NekoSilverFox <weidufox@gmail.com>
2023-09-21 21:20:36 +0300

    增加 MPI_Allredece 的示例和笔记

--- MPI/53_组通讯_规约_Reduce/mpi_reduce.cpp
+++ MPI/53_组通讯_规约_Reduce/mpi_reduce.cpp
@@ -12,7 +12,8 @@ int main(int argc, char* argv[])
     int data = rank;
     printf("data in rank %d is %d\n", rank, data);
 
-    MPI_Reduce(&data, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); // 所有进程都要调用，并不是只有根进程。但只有根进程的结果是对的
+    // MPI_Reduce(&data, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); // 所有进程都要调用，并不是只有根进程。但只有根进程的结果是对的
+    MPI_Allreduce(&data, &res, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);  // 所有进程都会获得最终的结果
 
     printf("Result reduce in rank %d is %d\n", rank, res);
 
