51fffbfa6e5456e80ec64c3573827327fa83ae47
NekoSilverFox <weidufox@gmail.com>
2023-09-23 21:11:29 +0300

    增加 lab 9 加密算法及广播秘钥

---
+++ MPI/90_SPbSTU_lab9_encrypt/lab9_Mengjianing.cpp
@@ -0,0 +1,103 @@
+/**
+ * @file lab9_Mengjianing.cpp
+ * @author MengJianing | Мэн Цзянин
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-22
+ * 
+ * @copyright Copyright (c) 2023 https://github.com/NekoSilverFox
+ * 
+ * 4. Криптономикон. В секретном институте No 127, находящемся где-то в Сибири, разработана новейшая система шифровки данных для малого бинеса.
+ *    Система представляет собой ряд модулей, каждый из которых принимает на вход часть сообщения и шифрует его по определенной схеме.
+ *    Какую именно часть текста шифрует каждый из модулей, задается специальным ключом, который меняется каждый день.
+ *    Написать программу, моделирующую работу шифровальной системы. В качестве шифров использовать прямые подстановки
+ *    (вид шифра, когда каждому символу исходного текста ставится в соответствие какойто символ шифрованного текста, причем всегда один и тот же).
+ *    В качестве ключа использовать подстановку, нижняя строка которой задает номер модуля, текст делится на равные части,
+ *    весь остаток получает тот модуль, который шифрует последнюю часть. Использовать метод передачи информации «точка-точка».
+ */
+
+#include <iostream>
+#include <vector>
+#include <mpi.h>
+#include <ctime>
+
+#define DEBUG               1
+const std::string str_dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,.";
+const int NUM_RANDOM_CHAR = str_dic.size(); //26 + 26 + 10 + 3; // A~Z + a~z + 0~9 + space,.
+const int ASCII_START     = 32;
+const int ASCII_END       = 127;
+const int TAG_KEY         = 0;
+
+/**
+ * @brief Получить зашифрованный словарь случайных чисел по текущей дате
+ * 
+ * @return std::string Словарь сопоставлений, необходимых для шифрования строк
+ */
+std::string get_today_key_dic()
+{
+    /* Получить текущую дату */
+    time_t cur_time;
+    time(&cur_time);
+    
+    tm* date = localtime(&cur_time);
+    int year = 1900 + date->tm_year;
+    int mon = 1 + date->tm_mon;
+    int day = date->tm_mday;
+    std::cout << "Time now: " << day << "-" << mon << "-" << year << "\n";
+    
+    /* Получить словарь случайных чисел, зашифрованных по текущему времени */
+    srand(day * 1000000 + mon * 10000 + year);
+    int i = 0;
+    char c;
+    std::string key_dic = "";
+    while (i < NUM_RANDOM_CHAR)
+    {
+        c = static_cast<char>(rand() % (ASCII_END - ASCII_START) + ASCII_START);
+        if (key_dic.find(c) == std::string::npos)
+        {
+            key_dic += c;
+            i++;
+        }
+    }
+
+    return key_dic;
+}
+
+int main(int argv, char* argc[])
+{
+    int size, rank;
+    MPI_Status status;
+    MPI_Init(&argv, &argc);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* Boardcast Словарь для шифрования */
+    std::string key_dic;
+    char* arrc_key_dic = new char[NUM_RANDOM_CHAR + 1];
+    if (0 == rank)
+    {
+        std::cout << "==================== lab9 ====================\n";
+
+        key_dic = get_today_key_dic();
+        arrc_key_dic = key_dic.data();
+
+        std::cout << "MPI size = " << size << "\n"
+                  << "str_dic = " << str_dic << "\tlength = " << str_dic.size() << "\n"
+                  << "key_dic = " << key_dic << "\tlength = " << key_dic.size() << "\n"
+                  << "==============================================\n";
+    }
+    MPI_Bcast(arrc_key_dic, NUM_RANDOM_CHAR + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+    key_dic = std::string(arrc_key_dic);
+    std::cout << "key_dic in rank " << rank << " = " << key_dic << "\tlength = " << key_dic.size() << "\n";
+    
+#if DEBUG
+    MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+    /* Передача строки для шифрования, метод передачи информации «точка-точка» */
+    std::string str_source = "ABCDE89o ,.";
+
+    // std::cout << (int)11/3 << "\t" << 11 % 3 << "\n";
+
+    MPI_Finalize();
+}
