ec5855fb401dcb11142556ad97674e5d955991cd
NekoSilverFox <weidufox@gmail.com>
2023-09-29 02:00:06 +0300

    完成 矩阵与向量的 MPI 并行乘法计算

--- MPI/92_SPbSTU_lab12_Matrix/lab12_MengJianing.cpp
+++ MPI/92_SPbSTU_lab12_Matrix/lab12_MengJianing.cpp
@@ -22,22 +22,20 @@
 #include <fstream>
 #include <mpi.h>
 
-#define DEBUG                     1
+#define DEBUG                     0
 #define GENERATE_NEW_MATRIX_VECTOR  1
 const int RANK_ROOT             = 0;
 const int RANDOM_SAND           = 5140903;
 const char SOURCE_MATRIX_FILE[] = "source_matrix";
 const char SOURCE_VECTOR_FILE[] = "source_vector";
+const char RESULT_FILE[]        = "result";
 const char SEPARATOR            = ' ';
 const int MATRIX_MAX_NUM        = 10;
 const int VECTOR_MAX_NUM        = 10;
-const int M                     = 10;
-const int N                     = 10;
-const int TAG_LENGTH_MATRIX_FILE_NAME = 0;
-const int TAG_MATRIX_FILE_NAME = 1;
-const int TAG_LENGTH_VECTOR_FILE_NAME = 2;
-const int TAG_VECTOR_FILE_NAME = 3;
-const int TAG_NUM_VETOR_EVERY_RANK = 4;
+const int M                     = 200;  // SET M HERE
+const int N                     = 150;  // SET N HERE
+const int TAG_RES               = 0;
+
 
 /**
  * @brief Generat random matrix
@@ -62,7 +60,7 @@ bool generateRadomMatrix2File(const char* file_name, const int m, const int n)
     {
         for (int col = 0; col < n; col++)
         {
-            fout << rand() % MATRIX_MAX_NUM << ((col + 1 == m)? '\n' : SEPARATOR);
+            fout << rand() % MATRIX_MAX_NUM << ((col + 1 == n)? '\n' : SEPARATOR);
         }
     }
     fout.close();
@@ -120,7 +118,6 @@ int main(int argc, char* argv[])
 {
     int size, rank;
     bool is_suc = false;
-    MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -140,12 +137,13 @@ int main(int argc, char* argv[])
         /* Generating random matrices */
         is_suc = generateRadomMatrix2File(SOURCE_MATRIX_FILE, M, N);
         if (!is_suc) return 1;
-        std::cout << "Rank " << rank << " successfully randomized the matrix " << M << "x" << N << " to file " << SOURCE_MATRIX_FILE << "\n";
+        std::cout << "Rank " << rank << " successfully randomized the matrix " << M << "x" << N << " to file `" << SOURCE_MATRIX_FILE << "`\n";
 
         /* Generating random vector */
         is_suc = generateRadomVector2File(SOURCE_VECTOR_FILE, N);
         if (!is_suc) return 1;
-        std::cout << "Rank " << rank << " successfully randomized the vector " << N << " to file " << SOURCE_VECTOR_FILE << "\n";
+        std::cout << "Rank " << rank << " successfully randomized the vector " << N << " to file `" << SOURCE_VECTOR_FILE << "`\n";
+        std::cout << "------------------------------------------------\n";
     }
 #endif
 
@@ -156,6 +154,7 @@ int main(int argc, char* argv[])
     char* matrix_file_name;
     int length_vector_file_name;
     char* vector_file_name;
+    int row_matrix = 0;
     int num_col_every_rank[size];   // Number of rows to be processed per process, index arr - rank
     if (0 == rank)
     {
@@ -175,7 +174,7 @@ int main(int argc, char* argv[])
             std::cerr << "[ERROR] Can not open file " << matrix_file_name << "\n";
             return 1;
         }
-        int row_matrix = 0;
+        
         std::string tmp;
         while (getline(fin, tmp))
         {
@@ -198,7 +197,7 @@ int main(int argc, char* argv[])
 
     }
 
-    /* Broadcast source files name and Number of rows to be processed per process */
+    /************* Broadcast source files name and Number of rows to be processed per process *************/
     MPI_Bcast(&length_vector_file_name, 1, MPI_INT, RANK_ROOT, MPI_COMM_WORLD);
     if (0 == rank)
     {
@@ -223,18 +222,19 @@ int main(int argc, char* argv[])
 
     MPI_Bcast(num_col_every_rank, size, MPI_INT, RANK_ROOT, MPI_COMM_WORLD);
 
-    /* Get vector */
+    /************************************ Get vector ************************************/
     std::string str_v;
     std::fstream finv(vector_file_name, std::ios::in);
     if (!vector_file_name)
     {
         std::cerr << "[ERROR] Can not open file " << matrix_file_name << "in rank " << rank << "\n";
+        return 1;
     }
     getline(finv, str_v);
     std::vector<int> v_vector = splitString(str_v, SEPARATOR);
     finv.close();
 
-    /* Move file ptr for local rank */
+    /************************** Move file ptr for local rank **************************/
     int i_ptr = 0;
     for (int i = 0; i < rank; i++)
     {
@@ -248,28 +248,71 @@ int main(int argc, char* argv[])
         --i_ptr;
     }
 
-    /* Compute matrix's line x vector */
+    /************************* Compute matrix's every col*vector *************************/
     std::vector<int> v_matrix_cur_line;
+    int local_arr_res[num_col_every_rank[rank]];
+    int tmp_sum;
     for (int i = 0; i < num_col_every_rank[rank]; i++)
     {
+        tmp_sum = 0;
         getline(finm, cur_line);
         v_matrix_cur_line = splitString(cur_line, SEPARATOR);
 
-        for (auto e : v_matrix_cur_line) std::cout << e << "-r" << rank << " ";
-        std::cout << "\n";
+        for (int j = 0; j < v_matrix_cur_line.size(); j++)
+        {
+            tmp_sum += (v_matrix_cur_line[j] * v_vector[j]);
+        }
+        local_arr_res[i] = tmp_sum;
     }
     finm.close();
     
-    
-    
+    /************************* Send result arr to root *************************/
+    int* arr_res;
+    MPI_Request request;
+    MPI_Status status;
+    MPI_Isend(local_arr_res, num_col_every_rank[rank], MPI_INT, RANK_ROOT, TAG_RES, MPI_COMM_WORLD, &request);
+
+    /***************************** Mix result arr *****************************/
+    if (0 == rank)
+    {
+        int* res_rank_buf;
+        int cur_i_arr_res = 0;
+        arr_res = new int[row_matrix];
+        for (int i = 0; i < size; i++)
+        {
+            res_rank_buf = new int[num_col_every_rank[i]];
+            MPI_Irecv(res_rank_buf, num_col_every_rank[i], MPI_INT, i, TAG_RES, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, &status);
+
+            for (int j = 0; j < num_col_every_rank[i]; j++)
+            {
+                arr_res[cur_i_arr_res + j] = res_rank_buf[j];
+            }
+            cur_i_arr_res += num_col_every_rank[i];
+        }
+
+        std::fstream fres(RESULT_FILE, std::ios::out);
+        if (!fres)
+        {
+            std::cerr << "[ERROR] Can not open file " << matrix_file_name << "in rank " << rank << "\n";
+            return 1;
+        }
+        
+        std::cout << "\n\e[92m>>>>>>> Final result: ";
+        for (int i = 0; i < row_matrix; i++)
+        {
+            std::cout << arr_res[i] << " ";
+            fres << arr_res[i] << " ";
+        }
+        fres.close();
+         std::cout << "\n########### Successfully wrote the result to the file `" << RESULT_FILE << "` ###########\e[0m\n";
+    }
+
 
 #if DEBUG
     std::cout << "=============================== rank " << rank << " ===============================\n"
-              << "length_matrix_file_name = " << length_matrix_file_name << "\n"
-              << "matrix_file_name = " << matrix_file_name << "\n\n"
-
-              << "length_vector_file_name = " << length_vector_file_name << "\n"
-              << "vector_file_name = " << vector_file_name  << "\n";
+              << "matrix_file_name = " << matrix_file_name << "\tlength_matrix_file_name = " << length_matrix_file_name << "\n"
+              << "vector_file_name = " << vector_file_name << "\tlength_vector_file_name = " << length_vector_file_name << "\n";
         
     std::cout << "num_col_every_rank in rank " << rank << " is ";
     for (int i = 0; i < size; i++)
@@ -279,8 +322,15 @@ int main(int argc, char* argv[])
 
     std::cout << "\nv_vector in rank " << rank << " is ";
     for (auto i : v_vector) std::cout << i << " ";
-    std::cout << "\n";
+    std::cout << "\tsize = " << v_vector.size();
 #endif
+
+    std::cout << ">> Local result product in rank " << rank << ": ";
+    for (int i = 0; i < num_col_every_rank[rank]; i++)
+    {
+        std::cout << local_arr_res[i] << " ";
+    }
+    std::cout << "\n";
 
     MPI_Finalize();
 }
